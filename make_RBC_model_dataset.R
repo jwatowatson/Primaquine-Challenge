c780ef87c76999f84e7edd00e1e2cbf9899b443f
Rob Moss <robm.dev@gmail.com>
2024-06-18 12:57:41 +1000

    Adjust initial Hb data for individual ADPQ 2
    The Hb measurements on day 0 are clearly inconsistent with those on
    subsequent days. Accordingly, we replace these day 0 measurements with
    Hb measurements from the screening data. This has the following impact:
    
    - Haemocue hb: increased from 12.0 g/dL to 13.8 g/dL; and
    - CBC hb:      increased from 12.7 g/dL to 13.9 g/dL.

--- make_RBC_model_dataset.R
+++ make_RBC_model_dataset.R
@@ -1,47 +1,89 @@
-library(tidyverse)
+#!/usr/bin/env -S Rscript --vanilla
+#
+# Extract the study data, and adjust the initial Hb measurements
+# for individual "ADPQ 2" in the ascending dose study.
+#
 
-## load trial data
-load('Data/PQdata.rds')
-PQdat$Day = as.numeric(PQdat$Day)
+suppressPackageStartupMessages(library(tidyverse))
 
-PQdat = PQdat %>% filter(label != 'ADPQ 18', # subject 18 not included in analysis
-                         Day >= 0, Day <= 30) %>%
-  group_by(label, Day) %>%
-  mutate(Study_Day = unique(Day),
-         Haemocue_hb = mean(Haemocue_hb,na.rm=T),
-         CBC_hb = mean(CBC_hb, na.rm = T),
-         CBC_retic = mean(CBC_retic, na.rm=T),
-         Manual_retic = mean(Manual_retic, na.rm = T),
-         dose = sum(PQdose, na.rm = T)) %>%
-  ungroup()%>%
-  distinct(label, Day, .keep_all = T) %>%
-  arrange(label, Day) %>% group_by(label, Day) %>%
-  mutate(dosemgkg = dose/weight,
-         Mean_retic = mean(c(Manual_retic, CBC_retic), na.rm = T),
-         Mean_hb = mean(c(Haemocue_hb, CBC_hb), na.rm = T))
-PQdat$ID = ifelse(!is.na(PQdat$Ascending_ID), PQdat$Ascending_ID, PQdat$label)
-PQdat$ID2 = PQdat$label
+load_data <- function(fix_ADPQ2 = TRUE) {
+  load("Data/PQdata.rds")
+  PQdat$Day <- as.numeric(PQdat$Day)
 
-cols = c('ID', 'ID2', 'study', 'Study_Day','G6PD_variant','Haemocue_hb', 
-         'CBC_hb', 'CBC_retic', 'Manual_retic','Mean_hb','Mean_retic',
-         'dose', 'dosemgkg', 'weight')
+  # Individual "ADPQ 2" has clearly incorrect Hb measurements on day 0.
+  # These should be replaced by the pre-study values.
+  ADPQ2_initial_hb <- PQdat |>
+    filter(label == "ADPQ 2", Day < 0) |>
+    group_by(label) |>
+    summarise(
+      Haemocue_hb = mean(Haemocue_hb, na.rm = TRUE),
+      CBC_hb = mean(CBC_hb, na.rm = TRUE)
+    ) |>
+    select(! label)
 
-PQdat = PQdat[, cols]
-PQdat$CBC_hb = ifelse(is.nan(PQdat$CBC_hb), NA, PQdat$CBC_hb)
-PQdat$CBC_retic = ifelse(is.nan(PQdat$CBC_retic), NA, PQdat$CBC_retic)
-PQdat$Mean_retic = ifelse(is.nan(PQdat$Mean_retic), NA, PQdat$Mean_retic)
+  cols <- c(
+    "ID", "ID2", "study", "Study_Day", "G6PD_variant", "Haemocue_hb",
+    "CBC_hb", "CBC_retic", "Manual_retic", "Mean_hb", "Mean_retic",
+    "dose", "dosemgkg", "weight"
+  )
 
+  study_data <- PQdat |>
+    filter(
+      # Subject 18 is excluded from the analysis.
+      label != "ADPQ 18",
+      Day >= 0,
+      Day <= 30
+    ) |>
+    group_by(label, Day) |>
+    mutate(
+      Study_Day = unique(Day),
+      Haemocue_hb = mean(Haemocue_hb, na.rm = TRUE),
+      CBC_hb = mean(CBC_hb, na.rm = TRUE),
+      CBC_retic = mean(CBC_retic, na.rm = TRUE),
+      Manual_retic = mean(Manual_retic, na.rm = TRUE),
+      dose = sum(PQdose, na.rm = TRUE)
+    ) |>
+    ungroup() |>
+    distinct(
+      label,
+      Day,
+      .keep_all = TRUE
+    ) |>
+    arrange(label, Day) |>
+    group_by(label, Day) |>
+    mutate(
+      dosemgkg = dose / weight,
+      Mean_retic = mean(c(Manual_retic, CBC_retic), na.rm = TRUE),
+      Mean_hb = mean(c(Haemocue_hb, CBC_hb), na.rm = TRUE)
+    ) |>
+    ungroup() |>
+    mutate(
+      ID = ifelse(! is.na(Ascending_ID), Ascending_ID, label),
+      ID2 = label
+    ) |>
+    mutate_all(function(x) ifelse(is.nan(x), NA, x)) |>
+    select(all_of(cols))
 
-writeLines('Unique individuals:')
-unique(sort(PQdat$ID))
+  if (fix_ADPQ2) {
+    mask <- study_data$ID == "ADPQ 2" & study_data$Study_Day == 0
 
-writeLines('Unique IDs with repeats:')
-unique(sort(PQdat$ID2))
+    for (column in names(ADPQ2_initial_hb)) {
+      study_data[[column]][mask] <- ADPQ2_initial_hb[[column]]
+    }
+  }
 
+  writeLines("Unique individuals:")
+  print(unique(sort(study_data$ID)))
 
-save(PQdat, file = 'Data/RBC_model_data.RData')
+  writeLines("Unique IDs with repeats:")
+  print(unique(sort(study_data$ID2)))
 
+  output_file <- file.path("Data", "RBC_model_data.RData")
+  cat("Saving to", output_file, "...\n")
+  PQdat <- study_data
+  save(PQdat, file = output_file)
 
-## Setup models
+  study_data
+}
 
-
+PQdat <- load_data(fix_ADPQ2 = TRUE)
